3adb9ede2e45e4fcac8203a3a88251d53b8ac8b9
mohamadrezamohammadzade-a11y <mohamadrezamohammadzade@gmail.com>
2026-07-14 23:26:12 +0330

    Update telegram_service.py

--- telegram_service.py
+++ telegram_service.py
@@ -1,13 +1,14 @@
+import os
 from telegram import Bot
-from config import *
-import asyncio
 
-bot = Bot(token=TELEGRAM_BOT_TOKEN)
+BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
+CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
+
+bot = Bot(token=BOT_TOKEN)
 
 
 async def send_message(text):
-
     await bot.send_message(
-        chat_id=TELEGRAM_CHAT_ID,
-        text=text
+        chat_id=CHAT_ID,
+        text=text,
     )
